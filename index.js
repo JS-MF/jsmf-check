4b24801d6abbec16cb1ab37fea35a5e697e5eb17
Nicolas Biri <nicolas.biri@list.lu>
2016-01-25 11:19:58 +0100

    Documentation added

--- index.js
+++ index.js
@@ -2,32 +2,56 @@
 
 var _ = require('lodash');
 
+/** @constructor
+ * Initialize a rule checker
+ */
 function Checker() {
     this.selections = {};
     this.rules = {};
 }
 
+/** Add a rule to the checker
+ * @param {String} name - The name of the rule (used as a key by the checker: two rules can't have the same name)
+ * @param {Selection[]} selections - An array of selections used for to check that rule.
+ * @param {Function} test - The function that shold be evaluated to true for this rul to pass.
+ */
 Checker.prototype.addRule = function(name, selections, test) {
     this.rules[name] = new Rule(selections, test);
 }
 
+/** @consructor Create a rule.
+ * @param {Selection[]} selections - An array of selections used for to check that rule.
+ * @param {Function} test - The function that shold be evaluated to true for this rul to pass.
+ */
 function Rule(selections, check) {
     this.selections = selections;
     this.check = check;
 }
 
+/** An alternative to the rul constructor: takes the selections as the first parameters and the evaluation function as the last one
+ *
+ *  ie.: `Rule.define(arg1, arg2, ..., argn) = new Rule([arg1,...argn-1], argn)`
+ * */
 Rule.define = function() {
     var args = Array.prototype.slice.call(arguments);
     var check = args.pop();
     return new Rule(args, check);
 }
 
+/** @constructor The result of a rule evaluation
+ * @param {Object[]} errors - The errors encontered during the rule validation
+ * @member {boolean} succeed - Claims if the rule is valid or not (it succeed if it has no error).
+ */
 function RuleResult(errors) {
     this.errors = errors;
     this.succeed = _.isEmpty(errors);
 }
 
+/** Create a success RuleResult (without error) */
 RuleResult.success = new RuleResult([]);
+/** Create a single error RuleResult
+ * @param e - The error.
+ */
 RuleResult.error = function(e) {return new RuleResult([e]);}
 
 function mergeRuleResults(r0, r1) {
@@ -45,14 +69,17 @@ function Selection(type, f) {
     this.resolved = !(f instanceof Reference || f instanceof Function);
 }
 
+/** Create a "for all quantified" selection */
 Selection.all = function(f) {
     return new Selection(Selection.all, f);
 }
 
+/** Create an "exists quantified" selection */
 Selection.any = function(f) {
     return new Selection(Selection.any, f);
 }
 
+/** Create a selection that will be passed as-is, to the test function */
 Selection.raw = function(f) {
     return new Selection(Selection.raw, f);
 }
@@ -77,6 +104,12 @@ function resolveSelection(selection, input, selections) {
     }
 }
 
+/** Run the checker on the provided input.
+ *
+ * It launch each rules on the input and gather the results.
+ *
+ * @param input - The input to validate
+ */
 Checker.prototype.run = function(input) {
     var selections = _.mapValues(this.selections, function (s) { return s(input); });
     return _.reduce(
@@ -95,6 +128,10 @@ Checker.prototype.runOnTransformation = function(input, output) {
 }
 
 
+/** Run a single rule
+ * @param input - The input on which the rule is runned
+ * @param selections - A set of resolved selections, if required.
+ */
 Rule.prototype.run = function(input, selections) {
     selections = selections === undefined ? {} : selections;
     var resolvedSelections = _.map(
