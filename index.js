7e3e23238fc123505c84076caea3e44e29e2f8dc
Nicolas Biri <nicolas.biri@list.lu>
2016-06-06 10:09:06 +0200

    Add mocha to dev dependencies / format code

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-/* eslint es6 */
 'use strict'
 
 const _ = require('lodash')
@@ -7,8 +6,8 @@ const _ = require('lodash')
  * Initialize a rule checker
  */
 function Checker() {
-    this.helpers = {}
-    this.rules = {}
+  this.helpers = {}
+  this.rules = {}
 }
 
 /** Add a rule to the checker
@@ -17,7 +16,7 @@ function Checker() {
  * @param {Function} test - The function that shold be evaluated to true for this rul to pass.
  */
 Checker.prototype.addRule = function(name, selections, test) {
-    this.rules[name] = new Rule(selections, test)
+  this.rules[name] = new Rule(selections, test)
 }
 
 /** @consructor Create a rule.
@@ -25,8 +24,8 @@ Checker.prototype.addRule = function(name, selections, test) {
  * @param {Function} test - The function that shold be evaluated to true for this rul to pass.
  */
 function Rule(selections, check) {
-    this.selections = selections
-    this.check = check
+  this.selections = selections
+  this.check = check
 }
 
 /** An alternative to the rul constructor: takes the selections as the first parameters and the evaluation function as the last one
@@ -34,9 +33,9 @@ function Rule(selections, check) {
  *  ie.: `Rule.define(arg1, arg2, ..., argn) = new Rule([arg1,...argn-1], argn)`
  * */
 Rule.define = function() {
-    var args = Array.prototype.slice.call(arguments)
-    var check = args.pop()
-    return new Rule(args, check)
+  var args = Array.prototype.slice.call(arguments)
+  var check = args.pop()
+  return new Rule(args, check)
 }
 
 /** @constructor The result of a rule evaluation
@@ -44,19 +43,19 @@ Rule.define = function() {
  * @member {boolean} succeed - Claims if the rule is valid or not (it succeed if it has no error).
  */
 function RuleResult(errors) {
-    this.errors = errors
-    this.succeed = _.isEmpty(errors)
+  this.errors = errors
+  this.succeed = _.isEmpty(errors)
 }
 
 /** Create a success RuleResult (without error) */
 RuleResult.success = new RuleResult([])
-/** Create a single error RuleResult
- * @param e - The error.
- */
+  /** Create a single error RuleResult
+   * @param e - The error.
+   */
 RuleResult.error = (e => new RuleResult([e]))
 
 function mergeRuleResults(r0, r1) {
-    return new RuleResult(mergeErrors(r0.errors, r1.errors))
+  return new RuleResult(mergeErrors(r0.errors, r1.errors))
 }
 
 function mergeErrors(e0, e1) {
@@ -68,36 +67,36 @@ function mergeErrors(e0, e1) {
 }
 
 function ContextualReference(f) {
-    this.selector = f
+  this.selector = f
 }
 
 function Reference(name) {
-    this.ref = name
+  this.ref = name
 }
 
 function Selection(type, f) {
-    this.type = type
-    this.content = f
-    this.resolved = !(f instanceof Reference || f instanceof Function)
+  this.type = type
+  this.content = f
+  this.resolved = !(f instanceof Reference || f instanceof Function)
 }
 
 /** Create a "for all quantified" selection */
 Selection.forall = Selection.all = function(f) {
-    return new Selection(Selection.all, f)
+  return new Selection(Selection.all, f)
 }
 
 /** Create an "exists quantified" selection */
 Selection.exists = Selection.any = function(f) {
-    return new Selection(Selection.any, f)
+  return new Selection(Selection.any, f)
 }
 
 /** Create a selection that will be passed as-is, to the test function */
 Selection.raw = function(f) {
-    return new Selection(Selection.raw, f)
+  return new Selection(Selection.raw, f)
 }
 
 function resolved(selection, content) {
-    return new Selection(selection.type, content)
+  return new Selection(selection.type, content)
 }
 
 function resolveSelection(selection, input, selections) {
@@ -105,10 +104,10 @@ function resolveSelection(selection, input, selections) {
     return selection
   } else if (selection.content instanceof Reference) {
     const newContent = selections[selection.content.ref]
-      return resolved(selection, newContent)
+    return resolved(selection, newContent)
   } else if (selection.content instanceof Function) {
     const newContent = selection.content(input)
-      return resolved(selection, newContent)
+    return resolved(selection, newContent)
   } else {
     throw new Error(`invalid selection: ${selection}`)
   }
@@ -121,23 +120,25 @@ function resolveSelection(selection, input, selections) {
  * @param input - The input to validate
  */
 Checker.prototype.run = function(input) {
-    const selections = _.mapValues(this.helpers, s => s(input))
-    const result = _(this.rules)
-      .map((r, name) => {
-         const result = r.run(input, selections)
-         if (!_.isEmpty(result.errors)) {
-            const errors = result.errors
-            result.errors = {}
-            result.errors[name] = errors
-         }
-         return result
-      }).reduce(mergeRuleResults, RuleResult.success)
-    if (!_.isEmpty(result.errors)) { result.errors = Object.assign.apply(undefined, result.errors) }
-    return result
+  const selections = _.mapValues(this.helpers, s => s(input))
+  const result = _(this.rules)
+    .map((r, name) => {
+      const result = r.run(input, selections)
+      if (!_.isEmpty(result.errors)) {
+        const errors = result.errors
+        result.errors = {}
+        result.errors[name] = errors
+      }
+      return result
+    }).reduce(mergeRuleResults, RuleResult.success)
+  if (!_.isEmpty(result.errors)) {
+    result.errors = Object.assign.apply(undefined, result.errors)
+  }
+  return result
 }
 
 Checker.prototype.runOnTransformation = function(input, output) {
-    return this.run({in: input, out: output})
+  return this.run({in: input, out: output})
 }
 
 
@@ -146,54 +147,62 @@ Checker.prototype.runOnTransformation = function(input, output) {
  * @param references - A key-value map, used to resolve references
  */
 Rule.prototype.run = function(input, selections) {
-    selections = selections === undefined ? {} : selections
-    const resolvedSelections =
-      _.map(this.selections, s => resolveSelection(s, input, selections))
-    return check(this.check, resolvedSelections, [], [])
+  selections = selections === undefined ? {} : selections
+  const resolvedSelections =
+    _.map(this.selections, s => resolveSelection(s, input, selections))
+  return check(this.check, resolvedSelections, [], [])
 }
 
 function check(f, selections, context, path) {
-    let e = selections.shift()
-    if (e == undefined) { return f.apply(undefined, context) ? RuleResult.success : RuleResult.error(path) }
-    if (e.content instanceof ContextualReference) {
-        selections = selections.slice()
-        e = {type: e.type, content: e.content.selector.call(context)}
-    }
-    switch (e.type) {
-        case Selection.any:
-          path.push(e.content)
-          const test = _.some(e.content, x => {
-            const ctx = context.slice()
-            ctx.push(x)
-            return check(f, selections.slice(), ctx, []).succeed
-          })
-          return test ? RuleResult.success : RuleResult.error(path)
-        case Selection.all:
-          return _.reduce(e.content,
-              (res, x) => {
-                  const ctx = context.slice()
-                  ctx.push(x)
-                  const currentPath = path.slice()
-                  currentPath.push(x)
-                  const stack = selections.slice()
-                  return mergeRuleResults(res, check(f, stack, ctx, currentPath))
-              },
-              RuleResult.success)
-        case Selection.raw:
-          context.push(e.content)
-          path.push(e.content)
-          return check(f, selections, context, path)
-        default:
-          throw new Error(`Invalid selection type: ${e.type}`)
-    }
+  let e = selections.shift()
+  if (e == undefined) {
+    return f.apply(undefined, context) ?
+      RuleResult.success :
+      RuleResult.error(path)
+  }
+  if (e.content instanceof ContextualReference) {
+    selections = selections.slice()
+    e = {type: e.type, content: e.content.selector.call(context)}
+  }
+  switch (e.type) {
+  case Selection.any: {
+    path.push(e.content)
+    const test = _.some(e.content, x => {
+      const ctx = context.slice()
+      ctx.push(x)
+      return check(f, selections.slice(), ctx, []).succeed
+    })
+    return test ? RuleResult.success : RuleResult.error(path)
+  }
+  case Selection.all: {
+    return _.reduce(e.content,
+      (res, x) => {
+        const ctx = context.slice()
+        ctx.push(x)
+        const currentPath = path.slice()
+        currentPath.push(x)
+        const stack = selections.slice()
+        return mergeRuleResults(res, check(f, stack, ctx, currentPath))
+      },
+      RuleResult.success)
+  }
+  case Selection.raw: {
+    context.push(e.content)
+    path.push(e.content)
+    return check(f, selections, context, path)
+  }
+  default: {
+    throw new Error(`Invalid selection type: ${e.type}`)
+  }
+  }
 }
 
 function composeCheckers() {
-    const checkers = Array.prototype.slice.call(arguments)
-    const res = new Checker()
-    res.rules = _(checkers).map('rules').reduce((x,y) => Object.assign(x,y))
-    res.helpers = _(checkers).map('helpers').reduce((x,y) => Object.assign(x,y))
-    return res
+  const checkers = Array.prototype.slice.call(arguments)
+  const res = new Checker()
+  res.rules = _(checkers).map('rules').reduce((x,y) => Object.assign(x,y))
+  res.helpers = _(checkers).map('helpers').reduce((x,y) => Object.assign(x,y))
+  return res
 }
 
 function onInput(f) { return (x => f(x.in)) }
@@ -210,5 +219,5 @@ module.exports = {
   onInput,
   onOutput,
   Reference: function(x) {return new Reference(x)},
-  ContextualReference: function(x) {return new ContextualReference(x)},
+  ContextualReference: function(x) {return new ContextualReference(x)}
 }
